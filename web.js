b2b8a559d1f66d79db077d2174d618a8d2dabef9
ph81 <paperhearts81@gmail.com>
2013-07-08 02:41:57 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var outfile = "index.html";
+var buffer = new buffer (256);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.send(buffer.toString(fs.readFileSync(outfile, 'utf-8'));
+ });
+ 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {

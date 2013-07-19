52303d5e224143122b2aa569c4887a20da92fad7
ph81 <paperhearts81@gmail.com>
2013-07-19 06:16:48 +0000

    Updated file

--- web.js
+++ web.js
@@ -4,12 +4,14 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8'); 
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
   response.send(buf.toString());
  });
  
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
